0e0bd3196d945eabec6c2dfab485caeed1e456bc
karanveer1122 <91868785+karanveer1122@users.noreply.github.com>
2023-11-20 17:08:15 +0530

    slider visibility issue fixed
    slider becomes visible only when the height of the search box is increased

--- src/UI/Search.jsx
+++ src/UI/Search.jsx
@@ -12,13 +12,17 @@ const Search = () => {
       <input type='search' className='search-box' style={showFilter?{height:"100px"}:{}} placeholder='Search Here...'/>
       <div className='search-filter-icon'><img src="/assets/imgs/mi_filter.png" onClick={handleShowFilter}/></div>
       {showFilter && <div className='search-slider-container'>
-        <Box sx={{ width: 300 }}>
+        <Box sx={{ 
+          width: 300,
+        }}>
           <Slider
             size="small"
             defaultValue={70}
             aria-label="Small"
-            valueLabelDisplay="auto"
-            sx={{ color: '#656565' }}
+            valueLabelDisplay="on"
+            sx={{ 
+              color: '#656565',
+            }}
           />
         </Box>
       </div>}
