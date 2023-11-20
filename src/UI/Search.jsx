e729ee4e93e1867e8c9e78f33d6adba9b8124e04
karanveer1122 <91868785+karanveer1122@users.noreply.github.com>
2023-11-20 15:47:32 +0530

    pesudo element in input type search

---
+++ src/UI/Search.jsx
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react'
+const Search = () => {
+  return <Fragment>
+    <input type='search' className='search-box' placeholder='Search Here...'/>
+  </Fragment>
+}
+
+export default Search
---
+++ src/components/Tag.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react'
+
+const Tag = () => {
+  return <Fragment>
+    
+  </Fragment>
+}
+
+export default Tag
